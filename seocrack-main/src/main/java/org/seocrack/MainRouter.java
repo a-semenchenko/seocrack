aeb7347153e164622e21a5a9919b374378b1bf27
a.semenchenko <a.semenchenko@accenture.com>
2017-03-20 15:52:51 +0300

    servlets

---
+++ seocrack-main/src/main/java/org/seocrack/MainRouter.java
@@ -0,0 +1,19 @@
+package org.seocrack;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by a.semenchenko on 20.03.2017.
+ */
+public class MainRouter extends HttpServlet {
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  }
+
+  protected void goPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  }
+}
